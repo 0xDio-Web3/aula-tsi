b5b3960306c6a4fb625b15a8add72e69d7a2d346
0xDio <54447539+0xDio-Web3@users.noreply.github.com>
2025-11-13 20:31:05 -0300

    alter

---
+++ app.js
@@ -0,0 +1,9 @@
+function saudacao(){
+const nome = "aluno"
+const data = new Date().toLocaleString("pt-BR");
+console.log('ola, ${nome}')
+console.log('Este script está rodando dentro de um container Docker');
+console.log('Horario da execução ${data}');
+}
+
+saudacao();
